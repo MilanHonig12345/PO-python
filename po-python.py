11da96d35b1f6e9e0725a531f6e76f97dc1494b4
Milan <204636@stmichaelcollege.nl>
2026-02-12 13:43:57 +0100

    startscherm

--- po-python.py
+++ po-python.py
@@ -1,1 +1,26 @@
-print("abc")
+def startscherm():
+    print("-----------------------------")
+    print("=== BUNKER SURVIVAL ===")
+    print("1. Uitleg")
+    print("2. start")
+    print("-----------------------------")
+
+    kies = input("Kies 1 of 2: ")
+
+    if kies == "2":
+        print("Het spel begint")
+    else:
+        print("--------------------------------")
+        print("Er is een zombie-apocalyps uitgebroken.")
+        print("De wereld zoals je die kende bestaat niet meer.")
+        print("Jij hebt een bunker gevonden, maar veilig ben je nog niet.")
+
+        print("In dit spel moet je de juiste keuzes maken om te overleven.")
+        print("Verkeerde keuzes kosten levens.")
+        print("Als je levens op zijn, is het spel voorbij.")
+        print("Je kunt naar buiten voor spullen, maar daar zijn zombies.")
+        print("--------------------------------")
+        input("Druk op Enter om terug te gaan...")
+        startscherm()
+    
+startscherm()
